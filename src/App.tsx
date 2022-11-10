086b8577dcdfb47325fa045c4417e57c27c53f88
BruhovaAlena <bruhovaalena@gmail.com>
2022-11-10 14:16:39 +0100

    test requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [users, setUsers] = useState<string[]>([]);
+  console.log('users', users);
+  const getAllDiary = async () => {
+    try {
+      const response = await axios({
+        method: 'get',
+        baseURL: 'http://localhost:5000/',
+        url: '/api',
+      });
+      const userss = response.data.users;
+      setUsers(userss);
+    } catch (error) {
+      console.error('error', error);
+    }
+  };
+
+  useEffect(() => {
+    const getUsers = async () => {
+      await getAllDiary();
+    };
+    getUsers();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,7 +40,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {users.map((user) => (
+            <p>{user}</p>
+          ))}
         </a>
       </header>
     </div>
